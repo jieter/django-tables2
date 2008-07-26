3c7c438b99a986b0035914616e00561c4c1a7eef
Michael Elsdoerfer <michael@elsdoerfer.info>
2008-07-27 01:55:17 +0200

    added distutils setup script

---
+++ setup.py
@@ -0,0 +1,32 @@
+import os
+from distutils.core import setup
+
+def find_packages(root):
+    # so we don't depend on setuptools; from the Storm ORM setup.py
+    packages = []
+    for directory, subdirectories, files in os.walk(root):
+        if '__init__.py' in files:
+            packages.append(directory.replace(os.sep, '.'))
+    return packages
+
+setup(
+    name = 'django-tables',
+    version = '0.1',
+    description = 'Render QuerySets as tabular data in Django.',
+    author = 'Michael Elsdoerfer',
+    author_email = 'michael@elsdoerfer.info',
+    license = 'BSD',
+    url = 'http://launchpad.net/django-tables',
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        ],
+    packages = find_packages('django_tables'),
+)
