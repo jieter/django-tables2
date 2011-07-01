e51e32b0dfeaeb0e520909c16fc5e23775693385
Sławek Ehlert <slafs@op.pl>
2011-07-01 12:32:06 +0200

    show that it works with verbose_name and gettext

--- example/app/models.py
+++ example/app/models.py
@@ -1,10 +1,13 @@
+#encoding: utf-8
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 
 
 class Country(models.Model):
     """Represents a geographical Country"""
     name = models.CharField(max_length=100)
-    population = models.PositiveIntegerField()
+    population = models.PositiveIntegerField(verbose_name=_(u'# of people'))
     tz = models.CharField(max_length=50)
     visits = models.PositiveIntegerField()
 
--- example/app/tables.py
+++ example/app/tables.py
@@ -1,8 +1,8 @@
 import django_tables2 as tables
-
+from django.utils.translation import ugettext_lazy as _
 
 class CountryTable(tables.Table):
-    name = tables.Column()
+    name = tables.Column( verbose_name = _(u'different name'))
     population = tables.Column()
     tz = tables.Column(verbose_name='Time Zone')
     visits = tables.Column()
