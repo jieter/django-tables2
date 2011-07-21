89474db8728c19b456781df87a276c1f5c806962
Bradley Ayers <bradley.ayers@gmail.com>
2011-07-22 01:21:00 +1000

    bumped version to v0.7.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-tables2',
-    version='0.7.0',
+    version='0.7.1',
     description='Table framework for Django',
 
     author='Bradley Ayers',
