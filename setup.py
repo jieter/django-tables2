716ba71c90c0caea2a02ec46d6f3040f70a6e212
Bradley Ayers <bradley.ayers@gmail.com>
2011-11-14 18:08:18 +1000

    Tweak tests to work nicer with Jenkins

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
 
     install_requires=['Django >=1.3'],
     tests_require=['Django >=1.3', 'Attest >=0.4', 'django-attest', 'fudge',
-                   'django-haystack'],
+                   'django-haystack', 'unittest-xml-reporting'],
 
     test_loader='attest:FancyReporter.test_loader',
     test_suite='tests.everything',
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,34 +1,16 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
 from attest import AssertImportHook, Tests
-
 # Django's django.utils.module_loading.module_has_submodule is busted
 AssertImportHook.disable()
 
-from django.conf import global_settings, settings
 
-# It's important to configure prior to importing the tests, as some of them
-# import Django's DB stuff.
-settings.configure(
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-        }
-    },
-    INSTALLED_APPS = [
-        'tests.testapp',
-        'django_tables2',
-        'haystack',
-    ],
-    ROOT_URLCONF = 'tests.testapp.urls',
-    TEMPLATE_CONTEXT_PROCESSORS = ['django.core.context_processors.request'] + list(global_settings.TEMPLATE_CONTEXT_PROCESSORS),
-    HAYSTACK_SEARCH_ENGINE = 'simple',
-    HAYSTACK_SITECONF = 'tests.testapp.search_sites'
-)
-
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.testapp.settings'
 from django.test.simple import DjangoTestSuiteRunner
 runner = DjangoTestSuiteRunner()
 runner.setup_databases()
+
 
 from .columns import columns
 from .config import config
@@ -38,4 +20,34 @@ from .rows import rows
 from .templates import templates
 from .utils import utils
 
+
 everything = Tests([columns, config, core, models, rows, templates, utils])
+
+
+# -----------------------------------------------------------------------------
+
+
+junit = Tests()
+
+@junit.test
+def make_junit_output():
+    import xmlrunner
+    runner = xmlrunner.XMLTestRunner(output=b'reports')
+    runner.run(everything.test_suite())
+
+
+# -----------------------------------------------------------------------------
+
+
+pylint = Tests()
+
+@pylint.test
+def make_pylint_output():
+    from os.path import expanduser
+    from pylint.lint import Run
+    from pylint.reporters.text import ParseableTextReporter
+    if not os.path.exists('reports'):
+        os.mkdir('reports')
+    with open('reports/pylint.report', 'wb') as handle:
+        args = ['django_tables2', 'example', 'tests']
+        Run(args, reporter=ParseableTextReporter(output=handle), exit=False)
