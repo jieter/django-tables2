3c5ecdcd3cbd1977346d1c9c0e764ba047f842c9
Chris Wilson <chris+github@qwirx.com>
2012-02-07 12:08:07 +0000

    Remove the "hack", give templates access to all context variables, make RenderTableNode.render() more readable.

--- django_tables2/templatetags/django_tables2.py
+++ django_tables2/templatetags/django_tables2.py
@@ -146,29 +146,33 @@ class RenderTableNode(Node):
 
     def render(self, context):
         table = self.table.resolve(context)
+        
         if not isinstance(table, tables.Table):
             raise ValueError("Expected Table object, but didn't find one.")
+        
         if "request" not in context:
             raise AssertionError(
                     "{% render_table %} requires that the template context"
                     " contains the HttpRequest in a 'request' variable,"
                     " check your TEMPLATE_CONTEXT_PROCESSORS setting.")
-        request = context['request']
-        context = RequestContext(request, {"table": table})
+        
+        context.update({"table": table})
+
         if self.template:
             template = self.template.resolve(context)
         else:
             template = table.template
+        
         if isinstance(template, basestring):
             template = get_template(template)
         else:
             # assume some iterable was given
             template = select_template(template)
+        
         try:
-            table.request = request  # HACK! :(
             return template.render(context)
         finally:
-            del table.request
+            context.pop()
 
 @register.tag
 def render_table(parser, token):
