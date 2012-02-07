91b9df5586635f7822d422b6174e6c20bb15cdc6
Bradley Ayers <bradley.ayers@gmail.com>
2012-02-07 11:06:42 +1000

    Fix broken import. Bump to v0.8.3

--- django_tables2/views.py
+++ django_tables2/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.core.exceptions import ImproperlyConfigured
 from django.views.generic.base import TemplateResponseMixin
-from django.views.generic.list import BaseListView
+from django.views.generic.list import ListView
 
 
 class SingleTableMixin(object):
