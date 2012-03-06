1775d0fedd2f0e933be2c551fab1d895c7d6e66f
Bradley Ayers <bradley.ayers@gmail.com>
2012-03-06 10:34:36 +1000

    Update test requirements, pylint + Attest >=0.5.3

--- setup.py
+++ setup.py
@@ -16,8 +16,9 @@ setup(
     include_package_data=True,  # declarations in MANIFEST.in
 
     install_requires=['Django >=1.3'],
-    tests_require=['Django >=1.3', 'Attest >=0.4', 'django-attest >=0.2.2',
-                   'fudge', 'django-haystack', 'unittest-xml-reporting'],
+    tests_require=['Django >=1.3', 'Attest >=0.5.3', 'django-attest >=0.2.2',
+                   'fudge', 'django-haystack', 'unittest-xml-reporting',
+                   'pylint'],
 
     test_loader='tests:loader',
     test_suite='tests.everything',
