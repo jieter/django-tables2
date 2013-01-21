1476d54c87c6b4b915c0b209fb7b25afdf885d16
Mike Fogel <mike@fogel.ca>
2013-01-21 14:33:50 -0800

    Tests for MultiTableView

--- tests/views.py
+++ tests/views.py
@@ -24,6 +24,11 @@ class SimpleTable(tables.Table):
         model = Region
 
 
+class SimpleTable2(tables.Table):
+    class Meta:
+        model = Region
+
+
 @views.test_if(USING_CBV)
 def view_should_support_pagination_options():
     for name in ("Queensland", "New South Wales", "Victoria", "Tasmania"):
@@ -40,7 +45,7 @@ def view_should_support_pagination_options():
 
 
 @views.test_if(USING_CBV)
-def should_support_explicit_table_data():
+def view_should_support_explicit_table_data():
     class SimpleView(DispatchHookMixin, tables.SingleTableView):
         table_class = SimpleTable
         table_data = [
@@ -54,3 +59,57 @@ def should_support_explicit_table_data():
     request = build_request('/')
     response, view = SimpleView.as_view()(request)
     assert view.get_table().paginator.num_pages == 3
+
+
+@views.test_if(USING_CBV)
+def multiview_should_support_pagination_options():
+    for name in ("Queensland", "New South Wales", "Victoria", "Tasmania"):
+        Region.objects.create(name=name)
+
+    class SimpleMultiView(DispatchHookMixin, tables.MultiTableView):
+        table_classes = {
+            'regions1': SimpleTable,
+            'regions2': SimpleTable2,
+        }
+        table_models = {
+            'regions1': Region,
+            'regions2': Region,
+        }
+        table_paginations = {
+            'regions1': {'per_page': 2},
+            # defaults to 'True' for non-specified
+        }
+        template_name = 'throwaway'  # needed for TemplateView
+
+    request = build_request('/')
+    response, view = SimpleMultiView.as_view()(request)
+    assert view.get_tables()['regions1'].paginator.num_pages == 2
+    assert view.get_tables()['regions2'].paginator.num_pages == 1
+
+
+@views.test_if(USING_CBV)
+def multiview_should_support_explicit_table_data():
+    class SimpleMultiView(DispatchHookMixin, tables.MultiTableView):
+        table_classes = {
+            'regions1': SimpleTable,
+            'regions2': SimpleTable2,
+        }
+        table_data = {
+            'regions1': [
+                {'name': 'Queensland'},
+                {'name': 'New South Wales'},
+                {'name': 'Victoria'},
+            ],
+            'regions2': [
+                {'name': 'Tasmania'},
+            ]
+        }
+        table_paginations = {
+            'regions1': {'per_page': 1},
+        }
+        template_name = 'throwaway'  # needed for TemplateView
+
+    request = build_request('/')
+    response, view = SimpleMultiView.as_view()(request)
+    assert view.get_tables()['regions1'].paginator.num_pages == 3
+    assert view.get_tables()['regions2'].paginator.num_pages == 1
