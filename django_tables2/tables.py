c76a2bea1eb80d6fda4871bc1ce173eef0f608d2
Ionel Cristian Mărieș <contact@ionelmc.ro>
2013-08-27 14:38:32 +0300

    Ooops, forgot the parens.

--- django_tables2/tables.py
+++ django_tables2/tables.py
@@ -46,11 +46,11 @@ class TableData(object):
                     )
                 else:
                     # really horrible, but this syntax is not supported on PY3 and would not work otherwise
-                    exec """raise ValueError, (
+                    exec("""raise ValueError, (
                         'data must be QuerySet-like (have count and '
                         'order_by) or support list(data) -- %s has '
                         'neither. Original exception: %s' % (type(data).__name__, ex)
-                    ), sys.exc_info()[2]"""
+                    ), sys.exc_info()[2]""")
 
     def __len__(self):
         if not hasattr(self, "_length"):
