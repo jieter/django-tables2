3f6fd129d931f3ea573c76e9a3579a2c9ac260e5
Jan Pieter Waagmeester <jieter@jieter.nl>
2016-01-11 10:23:13 +0100

    remove home grown total_ordering in favor of functools.total_ordering

--- django_tables2/utils.py
+++ django_tables2/utils.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 import inspect
 import warnings
+from functools import total_ordering
 from itertools import chain
 
 import six
@@ -453,34 +454,6 @@ class cached_property(object):  # pylint: disable=C0103
 funcs = (name for name in ('getfullargspec', 'getargspec') if hasattr(inspect, name))
 getargspec = getattr(inspect, next(funcs))
 del funcs
-
-
-def total_ordering(cls):
-    """Class decorator that fills in missing ordering methods"""
-    convert = {
-        '__lt__': [('__gt__', lambda self, other: not (self < other or self == other)),
-                   ('__le__', lambda self, other: self < other or self == other),
-                   ('__ge__', lambda self, other: not self < other)],
-        '__le__': [('__ge__', lambda self, other: not self <= other or self == other),
-                   ('__lt__', lambda self, other: self <= other and not self == other),
-                   ('__gt__', lambda self, other: not self <= other)],
-        '__gt__': [('__lt__', lambda self, other: not (self > other or self == other)),
-                   ('__ge__', lambda self, other: self > other or self == other),
-                   ('__le__', lambda self, other: not self > other)],
-        '__ge__': [('__le__', lambda self, other: (not self >= other) or self == other),
-                   ('__gt__', lambda self, other: self >= other and not self == other),
-                   ('__lt__', lambda self, other: not self >= other)]
-    }
-    roots = set(dir(cls)) & set(convert)
-    if not roots:
-        raise ValueError('must define at least one ordering operation: < > <= >=')
-    root = max(roots)       # prefer __lt__ to __le__ to __gt__ to __ge__
-    for opname, opfunc in convert[root]:
-        if opname not in roots:
-            opfunc.__name__ = str(opname)  # Py2 requires non-unicode, Py3 requires unicode.
-            opfunc.__doc__ = getattr(int, opname).__doc__
-            setattr(cls, opname, opfunc)
-    return cls
 
 
 def computed_values(d):
