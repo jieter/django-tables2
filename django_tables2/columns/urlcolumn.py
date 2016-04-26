53a9002a654c9f45d2b25f9cfe612203b800d9c8
Anton Samarchyan <desecho@gmail.com>
2016-04-26 18:49:41 -0400

    Update urlcolumn.py

--- django_tables2/columns/urlcolumn.py
+++ django_tables2/columns/urlcolumn.py
@@ -19,12 +19,27 @@ class URLColumn(BaseLinkColumn):
         >>> table.rows[0]["www"]
         u'<a href="http://google.com">http://google.com</a>'
 
+        >>> class CompaniesTable(tables.Table):
+        ...     www = tables.URLColumn(label='Google')
+        ...
+        >>> table = CompaniesTable([{"www": "http://google.com"}])
+        >>> table.rows[0]["www"]
+        u'<a href="http://google.com">Google</a>'
+
     Additional attributes for the ``<a>`` tag can be specified via
     ``attrs['a']``.
 
     """
+    def __init__(self, *args, **kwargs):
+        self.label = kwargs.pop('label', False)
+        super(URLColumn, self).__init__(*args, **kwargs)
+
     def render(self, value):
-        return self.render_link(value, value)
+        if self.label:
+            label = self.label
+        else:
+            label = value
+        return self.render_link(value, label)
 
     @classmethod
     def from_field(cls, field):
