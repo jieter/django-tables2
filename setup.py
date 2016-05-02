aa72d261dbea61b49a906bfec94f59e29b314d28
Jan Pieter Waagmeester <jieter@jieter.nl>
2016-05-02 11:05:41 +0200

    Single quotes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 # get version without importing
 with open('django_tables2/__init__.py', 'rb') as f:
-    VERSION = str(re.search('__version__ = "(.+?)"', f.read().decode('utf-8')).group(1))
+    VERSION = str(re.search('__version__ = \'(.+?)\'', f.read().decode('utf-8')).group(1))
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
