2805291b8f0c3520e936234d8654b5f15ff6f4ea
Neil Williams <codehelp@debian.org>
2017-08-07 09:39:38 -0400

    Make tablib optional in the tests.
    Closes: #468
    
    Signed-off-by: Neil Williams <codehelp@debian.org>

--- django_tables2/export/__init__.py
+++ django_tables2/export/__init__.py
@@ -1,5 +1,7 @@
-
-from .export import TableExport
-from .views import ExportMixin
-
-__all__ = ('TableExport', 'ExportMixin')
+try:
+    from tablib import Dataset
+    from .export import TableExport
+    from .views import ExportMixin
+    __all__ = ('TableExport', 'ExportMixin')
+except ImportError:
+    __all__ = ('',)
