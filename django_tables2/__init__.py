de6d5dcff6a157ac7645649e837646d2b0109b18
Jan Pieter Waagmeester <jieter@zostera.nl>
2017-12-14 14:19:32 +0100

    Bump version to 1.17.0

--- django_tables2/__init__.py
+++ django_tables2/__init__.py
@@ -9,7 +9,7 @@ from .utils import A
 from .views import SingleTableMixin, SingleTableView, MultiTableMixin
 
 
-__version__ = '1.16.0'
+__version__ = '1.17.0'
 
 __all__ = (
     'Table', 'TableBase',
