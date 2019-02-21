d0a26b7e57054666a7ebef528a47205e25e47d71
Igor Jerosimić <IgorCode@users.noreply.github.com>
2019-02-21 10:19:56 +0100

    Fixes issue with wrong time format for TimeColumn (#650)
    * Fixes issue with wrong time format for TimeColumn
    
    * Removed unused settings import

--- django_tables2/columns/timecolumn.py
+++ django_tables2/columns/timecolumn.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 from __future__ import absolute_import, unicode_literals
 
-from django.conf import settings
 from django.db import models
 
 from django_tables2.utils import ucfirst
@@ -24,7 +23,7 @@ class TimeColumn(TemplateColumn):
 
     def __init__(self, format=None, *args, **kwargs):
         if format is None:
-            format = settings.TIME_FORMAT
+            format = "TIME_FORMAT"
         template = '{{ value|date:"%s"|default:default }}' % format
         super(TimeColumn, self).__init__(template_code=template, *args, **kwargs)
 
