76678966cdf1fe147a236a2424a90306829c9aaf
Jan Pieter Waagmeester <jieter@zostera.nl>
2019-09-23 19:27:41 +0200

    Debugging errors in CI

--- tests/app/settings.py
+++ tests/app/settings.py
@@ -1,4 +1,10 @@
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "TEST": {"NAME": ":memory:"},
+    }
+}
 
 INSTALLED_APPS = [
     "tests.app",
