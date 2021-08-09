914eb172049f465db0f1f57a7fb7f23fd4e687fb
Jan Pieter Waagmeester <jieter@zostera.nl>
2021-08-09 15:52:44 +0200

    Upgrade dependencies of example

--- example/settings.py
+++ example/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": join(ROOT, "database.sqlite")}
 }
-
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
