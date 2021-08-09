a15cc0987d662fb058e5b7a9aba3a99c35277f3b
Jan Pieter Waagmeester <jieter@zostera.nl>
2021-08-09 16:22:40 +0200

    Improve error message if openpyxl is not installed
    fixes: #807

--- tests/app/settings.py
+++ tests/app/settings.py
@@ -1,4 +1,5 @@
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 INSTALLED_APPS = [
     "django.contrib.auth",
