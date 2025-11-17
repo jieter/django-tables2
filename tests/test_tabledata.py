c8c3a0936ca6606b7bdba3cb957dfc3ccffdaf96
Sjoerd Job Postmus <sjoerdjob@sjec.nl>
2025-11-17 08:22:11 +0100

    Do not generate error when table data model is subclass of table model (#1015)
    Fixes #1010

--- tests/test_tabledata.py
+++ tests/test_tabledata.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 from django_tables2 import Table
 from django_tables2.data import TableData, TableListData, TableQuerysetData
 
-from .app.models import Person, Region
+from .app.models import Person, PersonProxy, Region
 from .utils import build_request
 
 
@@ -108,13 +108,35 @@ class TableQuerysetDataTest(TestCase):
         table = Table(data=data)
         self.assertEqual(len(table.data), 10)
 
-    def test_model_mismatch(self):
+    def test_no_warning_model_identical(self):
         class MyTable(Table):
             class Meta:
                 model = Person
 
-        with warnings.catch_warnings(record=True):
+        with warnings.catch_warnings(record=True) as w:
+            MyTable(Person.objects.all())
+
+        self.assertEqual(len(w), 0)
+
+    def test_warning_model_mismatch(self):
+        class MyTable(Table):
+            class Meta:
+                model = Person
+
+        with warnings.catch_warnings(record=True) as w:
             MyTable(Region.objects.all())
+
+        self.assertEqual(len(w), 1)
+
+    def test_no_warning_model_subclass(self):
+        class MyTable(Table):
+            class Meta:
+                model = Person
+
+        with warnings.catch_warnings(record=True) as w:
+            MyTable(PersonProxy.objects.all())
+
+        self.assertEqual(len(w), 0)
 
     def test_queryset_union(self):
         for i in range(10):
